3b13cbd7e0b841cb2fee2e677f37344f2c12f8ad
jayNamgung <jaynamgung@gmail.com>
2024-02-28 13:01:42 +0900

    프로그래머스 입문 문제_20240228

---
+++ Programmers_240228.py
@@ -0,0 +1,39 @@
+'''
+정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return num1 + num2
+
+'''
+정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return num1 - num2
+
+
+'''
+정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return num1 * num2
+
+
+'''
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return int(num1 / num2)
+
+
+'''
+정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return int(num1 / num2 * 1000)
+
+
+'''
+정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return 1 if num1 == num2 else -1
