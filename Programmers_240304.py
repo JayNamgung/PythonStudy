41082e611d99250733b605f17a9df0a1e058a1c5
jayNamgung <jaynamgung@gmail.com>
2024-03-04 12:59:22 +0900

    프로그래머스 기초문제 3일차 마무리

---
+++ Programmers_240304.py
@@ -0,0 +1,53 @@
+'''
+정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+'''
+def solution(num1, num2):
+    return num1%num2
+
+
+
+'''
+문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+'''
+def solution(arr):
+    return "".join(arr)
+
+
+
+'''
+문자열 my_string과 정수 k가 주어질 때, my_string을 k번 반복한 문자열을 return 하는 solution 함수를 작성해 주세요.
+'''
+def solution(my_string, k):
+    return my_string*k
+
+
+'''
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+12 ⊕ 3 = 123
+3 ⊕ 12 = 312
+양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
+
+단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
+'''
+def solution(a, b):
+    compare_1 = str(a)+str(b)
+    compare_2 = str(b)+str(a)
+    
+    return int(compare_1) if compare_1 > compare_2 else int(compare_2)
+
+
+
+'''
+연산 ⊕는 두 정수에 대한 연산으로 두 정수를 붙여서 쓴 값을 반환합니다. 예를 들면 다음과 같습니다.
+
+12 ⊕ 3 = 123
+3 ⊕ 12 = 312
+양의 정수 a와 b가 주어졌을 때, a ⊕ b와 2 * a * b 중 더 큰 값을 return하는 solution 함수를 완성해 주세요.
+
+단, a ⊕ b와 2 * a * b가 같으면 a ⊕ b를 return 합니다.
+'''
+def solution(a, b):
+    compare_1 = str(a) + str(b)
+    compare_2 = 2*a*b
+    return int(compare_1) if int(compare_1) > compare_2 else int(compare_2)
