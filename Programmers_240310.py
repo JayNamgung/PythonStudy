9535829e427124c483308e3dbca3a13f984eb429
jayNamgung <jaynamgung@gmail.com>
2024-03-10 13:49:54 +0900

    프로그래머스 기초 5일차

---
+++ Programmers_240310.py
@@ -0,0 +1,9 @@
+'''
+두 정수 a, d와 길이가 n인 boolean 배열 included가 주어집니다.
+첫째항이 a, 공차가 d인 등차수열에서 included[i]가 i + 1항을 의미할 때,
+이 등차수열의 1항부터 n항까지 included가 true인 항들만 더한 값을 return 하는 solution 함수를 작성해 주세요.
+'''
+def solution(a, d, included):
+    return sum(a + d * i for i, flag in enumerate(included) if flag)
+
+print(solution(3,4,"[true, false, false, true, true]"))
