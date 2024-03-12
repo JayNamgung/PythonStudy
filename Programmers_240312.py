2016971015f80661edb0865f0b59c1f958dcfcc2
jayNamgung <jaynamgung@gmail.com>
2024-03-12 13:40:23 +0900

    프로그래머스 5일차 입문 마무리리

--- Programmers_240312.py
+++ Programmers_240312.py
@@ -38,3 +38,61 @@ def solution(num_list):
 
 print(solution([3, 4, 5, 2, 1]))
 print(solution([5, 7, 8, 3]))
+
+
+def square(x):
+    return x * x
+
+numbers = [1, 2, 3, 4, 5]
+squared = map(square, numbers)
+print(list(squared))
+
+
+
+'''
+머쓱이네 옷가게는 10만 원 이상 사면 5%, 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+'''
+def solution(price):
+    return int(price * 0.95) if price >= 100000 and price < 300000 else (int(price * 0.90) if price >= 300000 and price < 500000 else (int(price * 0.80) if price >= 500000 else int(price)))
+
+print(solution(150000))
+print(solution(5800000))
+
+
+
+'''
+머쓱이는 추운 날에도 아이스 아메리카노만 마십니다.
+아이스 아메리카노는 한잔에 5,500원입니다. 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때,
+머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+'''
+def solution(money):
+    return [money // 5500, money % 5500]
+
+print(solution(5500))
+print(solution(15000))
+
+
+
+'''
+머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다.
+이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+'''
+def solution(age):
+    return 2022-int(age)+1
+
+print(solution(40))
+print(solution(23))
+
+
+
+'''
+정수가 들어 있는 배열 num_list가 매개변수로 주어집니다.
+num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 solution 함수를 완성해주세요.
+'''
+def solution(num_list):
+    return num_list[::-1]
+
+print(solution([1, 2, 3, 4, 5]))
+print(solution([1, 1, 1, 1, 1, 2]))
+print(solution([1, 0, 1, 1, 1, 3, 5]))
