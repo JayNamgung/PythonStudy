72e438be9cb028be784c02f6355df1ce5cc1a962
jayNamgung <jaynamgung@gmail.com>
2024-03-13 17:31:35 +0900

    프로그래머스 기초 6일차 3번까지지

--- Programmers_240313.py
+++ Programmers_240313.py
@@ -48,3 +48,65 @@ n = 0
 control = "wsdawsdassw"
 
 print(basic_day6_2(n, control)) #result : -1
+
+
+
+'''
+정수 배열 numLog가 주어집니다.
+처음에 numLog[0]에서 부터 시작해 "w", "a", "s", "d"로 이루어진 문자열을 입력으로 받아 순서대로 다음과 같은 조작을 했다고 합시다.
+
+"w" : 수에 1을 더한다.
+"s" : 수에 1을 뺀다.
+"d" : 수에 10을 더한다.
+"a" : 수에 10을 뺀다.
+
+그리고 매번 조작을 할 때마다 결괏값을 기록한 정수 배열이 numLog입니다.
+즉, numLog[i]는 numLog[0]로부터 총 i번의 조작을 가한 결과가 저장되어 있습니다.
+
+주어진 정수 배열 numLog에 대해 조작을 위해 입력받은 문자열을 return 하는 solution 함수를 완성해 주세요.
+'''
+def basic_day6_3_1(numLog):
+    answer = []
+
+    for idx, val in enumerate(numLog):
+        compare_val = 0
+
+        if idx != 0:
+            compare_val = val - numLog[idx-1]
+        if compare_val == 1:
+            answer.append("w")
+        elif compare_val == -1:
+            answer.append("s")
+        elif compare_val == 10:
+            answer.append("d")
+        elif compare_val == -10:
+            answer.append("a")
+    return "".join(answer)
+
+
+def basic_day6_3_2(numLog):
+    answer = ''
+    data_dict = {1:"w", -1:"s", 10:"d", -10:"a"}
+
+    for idx in range(1, len(numLog)):
+        compare_val = numLog[idx] - numLog[idx - 1]
+        answer += data_dict[compare_val]
+    return answer
+
+
+def basic_day6_3_3(numLog):
+    answer = ''
+    data_dict = {1:"w", -1:"s", 10:"d", -10:"a"}
+
+    for idx, val in enumerate(numLog[1:], start=1):
+        compare_val = val - numLog[idx - 1]
+
+        if compare_val in data_dict:
+            answer += data_dict[compare_val]
+    return answer
+
+
+numLog = [0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1]
+print(basic_day6_3_1(numLog)) #result : "wsdawsdassw"
+print(basic_day6_3_2(numLog)) #result : "wsdawsdassw"
+print(basic_day6_3_3(numLog)) #result : "wsdawsdassw"
